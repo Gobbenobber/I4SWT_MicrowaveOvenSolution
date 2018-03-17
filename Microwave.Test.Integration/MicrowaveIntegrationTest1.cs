21d64686c45fe706d112bc7f95f33afa26b12046
Gobbenobber <gobbenobber@gmail.com>
2018-03-17 17:04:26 +0100

    Added integration tests 1 and 2.

---
+++ Microwave.Test.Integration/MicrowaveIntegrationTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    class MicrowaveIntegrationTest1
+    {
+
+        private IDoor _door;
+        private ICookController _cookController;
+        private ILight _light;
+        private IDisplay _display;
+        private IButton _timeButton;
+        private IButton _powerButton;
+        private IButton _cancelButton;
+        private IUserInterface _userInterface;
+        [SetUp]
+        public void SetUp()
+        {
+            _door = Substitute.For<IDoor>();
+            _cookController = Substitute.For<ICookController>();
+            _light = Substitute.For<ILight>();
+            _display = Substitute.For<IDisplay>();
+            _timeButton = new Button();
+            _powerButton = new Button();
+            _cancelButton = new Button();
+            _userInterface = new UserInterface(_powerButton,_timeButton,_cancelButton,_door,_display,_light,_cookController);
+        }
+
+        [Test]
+        public void PowerPressed_UICallsShowPower50InDisplay()
+        {
+            _powerButton.Press();
+            
+            _display.Received().ShowPower(50);
+        }
+
+        [Test]
+        public void PowerPressedTwice_UICallsShowPower100InDisplay()
+        {
+            _powerButton.Press();
+            _powerButton.Press();
+            _display.Received().ShowPower(100);
+        }
+
+        [Test]
+        public void PowerPressed_UICallsShowPowerInDisplay()
+        {
+            _powerButton.Press();
+
+            _display.Received().ShowPower(50);
+        }
+    }
+}
---
+++ Microwave.Test.Integration/MicrowaveIntegrationTest2.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    [TestFixture]
+    public class MicrowaveIntegrationTest2
+    {
+
+        private IDoor _uut;
+        private ICookController _cookController;
+        private ILight _light;
+        private IDisplay _display;
+        private IButton _timeButton;
+        private IButton _powerButton;
+        private IButton _cancelButton;
+        private IUserInterface _userInterface;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _uut = new Door();
+            _cookController = Substitute.For<ICookController>();
+            _light = Substitute.For<ILight>();
+            _display = Substitute.For<IDisplay>();
+            _timeButton = new Button();
+            _powerButton = new Button();        //Buttons can be included; assumed unit tested prior to integration test.
+            _cancelButton = new Button();
+            _userInterface = new UserInterface(_powerButton,_timeButton,_cancelButton,_uut,_display,_light,_cookController);
+        }
+
+        [Test]
+        public void DoorOpened_UITurnsOnLight()
+        {
+            _uut.Open();
+            _light.Received().TurnOn();
+        }
+
+        [Test]
+        public void DoorOpenedThenClosed_UITurnsLightOnOff()
+        {
+            _uut.Open();
+            _uut.Close();
+            Received.InOrder(() =>
+            {
+                _light.TurnOn();
+                _light.TurnOff();
+            });
+            
+        }
+
+        public void DoorOpenedWhileCooking_StopIsCalledOnCookController()
+        {
+            _powerButton.Press();
+            _powerButton.Press();
+            
+        }
+
+    }
+}
+
