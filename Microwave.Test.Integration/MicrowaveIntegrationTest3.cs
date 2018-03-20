10bb12b36ae66c8c07d5916a31730c2affd6c426
Gobbenobber <gobbenobber@gmail.com>
2018-03-20 10:10:17 +0100

    Added setup for IT3

--- Microwave.Test.Integration/MicrowaveIntegrationTest3.cs
+++ Microwave.Test.Integration/MicrowaveIntegrationTest3.cs
@@ -11,32 +11,39 @@ using NUnit.Framework;
 
 namespace Microwave.Test.Integration
 {
+    [TestFixture]
     class MicrowaveIntegrationTest3
     {
 
-        private IDoor _uut;
-        private ICookController _cookController;
+        private IDoor _door;
+        private ICookController _uut;
         private ILight _light;
+        private IPowerTube _powerTube;
         private IDisplay _display;
+        private ITimer _timer;
         private IButton _timeButton;
         private IButton _powerButton;
         private IButton _startCancelButton;
+        private IOutput _output;
         private IUserInterface _userInterface;
 
         [SetUp]
         public void SetUp()
         {
-            _uut = new Door();
+            _door = new Door();
             _light = Substitute.For<ILight>();
             _display = Substitute.For<IDisplay>();
             _timeButton = new Button();
             _powerButton = new Button();        //Buttons can be included; assumed unit tested prior to integration test.
             _startCancelButton = new Button();
-            _cookController = new CookController();
+            _powerTube = Substitute.For<IPowerTube>();
+            _output = Substitute.For<IOutput>();
+            _timer = Substitute.For<ITimer>();
+            _uut = new CookController(_timer,_display,_powerTube);
             _userInterface = new UserInterface(_powerButton, _timeButton, _startCancelButton, _uut, _display, _light, _cookController);
 
         }
 
-
+        
     }
 }
