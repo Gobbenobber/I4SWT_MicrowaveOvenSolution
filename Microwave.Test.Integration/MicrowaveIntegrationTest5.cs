2d16f928ccc9d4cdd8600e483feb4dda047c384b
Gobbenobber <gobbenobber@gmail.com>
2018-03-20 16:27:27 +0100

    Added IntegrationTest5

---
+++ Microwave.Test.Integration/MicrowaveIntegrationTest5.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    class MicrowaveIntegrationTest5
+    {
+        private IOutput _output;
+        private IDoor _door;
+        private ICookController _cookController;
+        private ILight _light;
+        private IPowerTube _powerTube;
+        private IDisplay _display;
+        private ITimer _timer;
+        private IButton _timeButton;
+        private IButton _powerButton;
+        private IButton _startCancelButton;
+        private IUserInterface _userInterface;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _door = new Door();
+            _timeButton = new Button();
+            _powerButton = new Button();
+            _startCancelButton = new Button();
+            _output = Substitute.For<IOutput>();
+            _powerTube = new PowerTube(_output);
+            _display = new Display(_output);
+            _light = new Light(_output);
+            _timer = new Timer();
+            _cookController = new CookController(_timer, _display, _powerTube);
+            _userInterface = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
+        }
+
+        [SetUp]
+        public void DerSkalTestesMeget()
+        {
+
+        }
+    }
+}
