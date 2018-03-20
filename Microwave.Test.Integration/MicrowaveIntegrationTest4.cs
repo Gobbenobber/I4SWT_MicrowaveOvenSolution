dc5676ad05631ef66c125ddfdccb662802e1a163
Gobbenobber <gobbenobber@gmail.com>
2018-03-20 16:16:51 +0100

    Added IT4 and wrote 1 test in it

--- Microwave.Test.Integration/MicrowaveIntegrationTest4.cs
+++ Microwave.Test.Integration/MicrowaveIntegrationTest4.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using MicrowaveOvenClasses.Boundary;
 using MicrowaveOvenClasses.Controllers;
@@ -12,10 +11,8 @@ using NUnit.Framework;
 
 namespace Microwave.Test.Integration
 {
-    [TestFixture]
-    class MicrowaveIntegrationTest3
+    class MicrowaveIntegrationTest4
     {
-
         private IDoor _door;
         private ICookController _uut;
         private ILight _light;
@@ -34,47 +31,29 @@ namespace Microwave.Test.Integration
             _light = Substitute.For<ILight>();
             _display = Substitute.For<IDisplay>();
             _timeButton = new Button();
-            _powerButton = new Button();       
+            _powerButton = new Button();        
             _startCancelButton = new Button();
             _powerTube = Substitute.For<IPowerTube>();
             _timer = Substitute.For<ITimer>();
-            _uut = new CookController(_timer,_display,_powerTube);
+            _uut = new CookController(_timer, _display, _powerTube);
             _userInterface = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _uut);
 
         }
 
         [Test]
-        public void StartCancelPressedAfterSetUp_TurnOnPowerTubeAndTimer()
+        public void DoorOpenedWhileCooking_TurnOffPowerTubeAndTimer()
         {
-            _powerButton.Press();
-            _timeButton.Press();
-            _startCancelButton.Press();
-            Received.InOrder(() =>
-            {
-                //_display.ShowTime(1, 0); //<-- Previously tested
-                //_light.TurnOn();
-                _powerTube.TurnOn(50);
-                _timer.Start(60);
-            });
-        }
 
-        [Test]
-        public void StartCancelPressedWhileCookinng_TurnOffPowerTubeAndTimer()
-        {
             _powerButton.Press();
             _timeButton.Press();
             _startCancelButton.Press();
-            _startCancelButton.Press();
+            _door.Open();
             Received.InOrder(() =>
             {
-                //_display.ShowTime(1, 0); //<-- Previously tested
-                //_light.TurnOn();
                 _powerTube.TurnOn(50);
                 _timer.Start(60);
                 _powerTube.TurnOff();
                 _timer.Stop();
-                //_light.TurnOff();
-                //_display.Clear();
             });
         }
     }
