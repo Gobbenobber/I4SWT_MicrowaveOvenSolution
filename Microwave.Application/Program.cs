cb0f52d50183a514975c44c8390d39518f8ab9ab
Gobbenobber <gobbenobber@gmail.com>
2018-03-21 16:00:57 +0100

    Finished test application?

--- Microwave.Application/Program.cs
+++ Microwave.Application/Program.cs
@@ -32,45 +32,97 @@ namespace Microwave.Application
             var _cookController = new CookController(_timer, _display, _powerTube);
             _cookController.UI = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
 
+            Console.WriteLine("============= IT7 - BUTTON IS DRIVER ===========");
 
-            Console.WriteLine("=============IT7===========");
             Console.WriteLine("=============Testíng normal use case===========");
-            _door.Open();
-            _door.Close();
             _powerButton.Press();
             _timeButton.Press();
             _startCancelButton.Press();
             Thread.Sleep(62000);
 
-            Console.WriteLine("=============Testíng StartCancelPressed while cooking===========");
-            _door.Open();
-            _door.Close();
+            Console.WriteLine("\n=============Testíng StartCancelPressed while cooking===========");
             _powerButton.Press();
             _timeButton.Press();
             _startCancelButton.Press();
             _startCancelButton.Press();
+            Thread.Sleep(1000);
 
-            Console.WriteLine("=============Testíng StartCancelPressed during setup===========");
-            _door.Open();
-            _door.Close();
+            Console.WriteLine("\n=============Testíng StartCancelPressed during setup===========");
+
+            // Setup all the objects, 
+            _output = new Output();
+            _door = new Door();
+            _timeButton = new Button();
+            _powerButton = new Button();
+            _startCancelButton = new Button();
+            _powerTube = new PowerTube(_output);
+            _display = new Display(_output);
+            _light = new Light(_output);
+            _timer = new Timer();
+            _cookController = new CookController(_timer, _display, _powerTube);
+            _cookController.UI = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
+
             _powerButton.Press();
             _startCancelButton.Press();
+            Thread.Sleep(1500);
 
-            Console.WriteLine("=============IT8===========");
-            Console.WriteLine("=============DoorOpenedWhileCooking===========");
-            _door.Open();
-            _door.Close();
+            Console.WriteLine("\n\n============= IT8 - DOOR IS DRIVER ===========");
+
+            Console.WriteLine("\n=============DoorOpenedWhileCooking===========");
+            // Setup all the objects, 
+            _output = new Output();
+            _door = new Door();
+            _timeButton = new Button();
+            _powerButton = new Button();
+            _startCancelButton = new Button();
+            _powerTube = new PowerTube(_output);
+            _display = new Display(_output);
+            _light = new Light(_output);
+            _timer = new Timer();
+            _cookController = new CookController(_timer, _display, _powerTube);
+            _cookController.UI = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
+
             _powerButton.Press();
             _timeButton.Press();
             _startCancelButton.Press();
             Thread.Sleep(10000);
             _door.Open();
 
-            Console.WriteLine("=============DoorOpenedDuringSetup===========");
-            _door.Open();
-            _door.Close();
+            Console.WriteLine("\n=============DoorOpenedDuringSetup===========");
+
+            // Setup all the objects, 
+            _output = new Output();
+            _door = new Door();
+            _timeButton = new Button();
+            _powerButton = new Button();
+            _startCancelButton = new Button();
+            _powerTube = new PowerTube(_output);
+            _display = new Display(_output);
+            _light = new Light(_output);
+            _timer = new Timer();
+            _cookController = new CookController(_timer, _display, _powerTube);
+            _cookController.UI = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
+
             _powerButton.Press();
             _door.Open();
+            
+            Console.WriteLine("\n=============DoorOpenedThenClosed===========");
+
+            // Setup all the objects, 
+            _output = new Output();
+            _door = new Door();
+            _timeButton = new Button();
+            _powerButton = new Button();
+            _startCancelButton = new Button();
+            _powerTube = new PowerTube(_output);
+            _display = new Display(_output);
+            _light = new Light(_output);
+            _timer = new Timer();
+            _cookController = new CookController(_timer, _display, _powerTube);
+            _cookController.UI = new UserInterface(_powerButton, _timeButton, _startCancelButton, _door, _display, _light, _cookController);
+
+            _door.Open();
+            _door.Close();
 
             // Wait while the classes, including the timer, do their job
             System.Console.WriteLine("Tast enter når applikationen skal afsluttes");
